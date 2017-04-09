fab2ce62cd450f54d88be91a58682f21df0aa9f6
flak <flak@flakhost.flakdomain>
2017-04-09 07:28:23 -0400

    foo

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ upDown = Servo("Second Servo")
 leftRight.attach(3)
 upDown.attach(5)
 
-for angle in range(45, 135):
+for angle in range(90, 135):
     leftRight.write(angle)
     time.sleep(0.005)
-for angle in range(135, 45, -1):
-    leftRight.write(angle)
-    time.sleep(0.005)
-
-for angle in range(45, 135):
+for angle in range(90, 80, -1):
     upDown.write(angle)
     time.sleep(0.005)
-for angle in range(135, 45, -1):
+
+for angle in range(135, 90, -1):
+    leftRight.write(angle)
+    time.sleep(0.005)
+for angle in range(80, 90, 1):
     upDown.write(angle)
     time.sleep(0.005)
