7f544686c81346e6b6fbdbc16dcf0801e08b90e5
flak <flak@flakhost.flakdomain>
2017-04-09 09:25:27 -0400

    foo

--- test.py
+++ test.py
@@ -9,11 +9,11 @@ upDown.attach(5)
 for angle in range(90, 110):
     leftRight.write(angle)
     time.sleep(0.005)
-for angle in range(70, 120):
+for angle in range(70, 140):
     upDown.write(angle)
     time.sleep(0.005)
 
-for angle in range(120, 70, -1):
+for angle in range(140, 70, -1):
     upDown.write(angle)
     time.sleep(0.005)
 for angle in range(110, 90, -1):
