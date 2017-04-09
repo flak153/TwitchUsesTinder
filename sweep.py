20ee54c8e84521e49e5b5a0e2c25a50c74d7dce3
flak <flak@flakhost.flakdomain>
2017-04-09 09:37:49 -0400

    foo

--- sweep.py
+++ sweep.py
@@ -22,21 +22,30 @@ from flask import Flask
 app = Flask(__name__)
 
 # Create a new servo object with a reference name
-myServo = Servo("First Servo")
+leftRight = Servo("First Servo")
+upDown = Servo("Second Servo")
 
 # Attaches the servo to pin 3 in Arduino Expansion board
-myServo.attach(3)
+leftRight.attach(3)
+upDown.attach(5)
 
 # Print servo settings
 
 
 @app.route('/left')
 def left():
-    for angle in range(90, 180):
-        myServo.write(angle)
+    for angle in range(90, 110):
+        leftRight.write(angle)
         time.sleep(0.005)
-    for angle in range(180, 90, -1):
-        myServo.write(angle)
+    for angle in range(70, 120):
+        upDown.write(angle)
+        time.sleep(0.005)
+
+    for angle in range(120, 70, -1):
+        upDown.write(angle)
+        time.sleep(0.005)
+    for angle in range(110, 90, -1):
+        leftRight.write(angle)
         time.sleep(0.005)
 
 
@@ -44,13 +53,18 @@ def left():
 
 @app.route('/right')
 def right():
-    for angle in range(90, 0, -1):
-        myServo.write(angle)
+    for angle in range(90, 110):
+        leftRight.write(angle)
+        time.sleep(0.005)
+    for angle in range(70, 120):
+        upDown.write(angle)
         time.sleep(0.005)
 
-        # From 180 to 0 degrees
-    for angle in range(0, 90,):
-        myServo.write(angle)
+    for angle in range(120, 70, -1):
+        upDown.write(angle)
+        time.sleep(0.005)
+    for angle in range(110, 90, -1):
+        leftRight.write(angle)
         time.sleep(0.005)
     return "Done"
 
